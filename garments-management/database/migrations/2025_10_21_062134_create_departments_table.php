bf2520e79473848e8dd03b034e77f5fe3716cbe5
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-11-01 10:56:47 +0600

    department fixed

--- garments-management/database/migrations/2025_10_21_062134_create_departments_table.php
+++ garments-management/database/migrations/2025_10_21_062134_create_departments_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('employee_no')->nullable();
             $table->timestamps();
         });
     }
